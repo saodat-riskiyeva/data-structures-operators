40f74375fb04ce35ee3b92501deb1baff70b0aca
Saodat <saodat.riskiyeva@gmail.com>
2024-05-24 00:10:00 -0400

    coding challenge

--- final/script.js
+++ final/script.js
@@ -615,6 +615,39 @@ for (const [i, el] of menu.entries()) {
 
 ///////////////////////////////////////
 // Coding Challenge #1
+// 1.
+const [players1, players2] = game.players;
+console.log(players1, players2);
+
+// 2.
+const [gk, ...fielsPlayers] = players1;
+console.log(gk, fielsPlayers);
+
+// 3.
+const allPlayers = [...players1, ...players2];
+console.log(allPlayers);
+
+// 4.
+const players1Final = [...players1, 'Thiago', 'Coutinho', 'Perisic'];
+console.log(players1Final);
+
+// 5.
+const {
+  odds: { team1, x: draw, team2 },
+} = game;
+console.log(team1, draw, team2);
+
+// 6.
+const printGoals = function (...players) {
+  console.log(`${players.length} goals were scored`);
+};
+// printGoals('Davies', 'Muller', 'Lewandowski', 'Kimmich');
+// printGoals('Davies', 'Muller');
+printGoals(...game.scored);
+
+// 7.
+team1 < team2 && console.log('Team 1 is more likely to win');
+team1 > team2 && console.log('Team 2 is more likely to win');
 
 /* 
 We're building a football betting app (soccer for my American friends 😅)!
